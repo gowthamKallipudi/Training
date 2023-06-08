a0dc04c9f8deaf0d2a947fb471c3bdf0f18f199a
gowthamKallipudi <gowthamkallipudi.k@vuedata.in>
2023-06-08 11:01:42 +0530

    updating the code with try resource block

--- Training/training1/Pattern.java
+++ Training/training1/Pattern.java
@@ -5,12 +5,38 @@ import java.util.*;
 class Pattern {
     
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Height: ");
-        int height = sc.nextInt();
-        System.out.print("Enter the character: ");
-        char symbol = sc.next().charAt(0);
-        pattern(height, symbol);
+
+        // Whenever we use try-with-resource and when we are exiting the try-catch block
+        // it automatically executes the close() method of AutoClosable interface
+        // the resources which are opened in the try(........) will get automatically closed
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the Height: ");
+            int height = sc.nextInt();
+            System.out.print("Enter the character: ");
+            char symbol = sc.next().charAt(0);
+            pattern(height, symbol);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+        // Without implementing try-with-resource block we can close the resource manually
+        // by declaring the resource variable as null outside try-catch block
+        // and we can close the resource in the finally block as follows
+
+        // Scanner sc = null;
+        // try {
+        //     sc = new Scanner(System.in);
+        //     System.out.print("Enter the Height: ");
+        //     int height = sc.nextInt();
+        //     System.out.print("Enter the character: ");
+        //     char symbol = sc.next().charAt(0);
+        //     pattern(height, symbol);
+        // } catch(Exception e) {
+        //     System.out.println(e);
+        // } finally {
+        //     sc.close();
+        // }
     }
 
     public static void pattern(int height, char symbol) {
