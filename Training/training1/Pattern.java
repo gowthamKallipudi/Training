9886616f5f1d35316a9cd84c13ef24a1753273e1
gowthamKallipudi <gowthamkallipudi.k@vuedata.in>
2023-06-09 12:51:03 +0530

    updated the code with some endcases and quality

--- Training/training1/Pattern.java
+++ Training/training1/Pattern.java
@@ -6,81 +6,7 @@ class Pattern {
 
   public static void main(String args[]) {
     Configuration config = argument2Configuration(args);
-
     pattern(config.getHeight(), config.getSymbol());
-    // Whenever we use try-with-resource and when we are exiting the try-catch block
-    // it automatically executes the close() method of AutoClosable interface
-    // the resources which are opened in the try(........) will get automatically closed
-
-    // if (args.length != 0) {
-    //   if (
-    //     (args[0].compareTo("-i") == 0 || args[0].compareTo("-input") == 0) &&
-    //     (args[1].compareTo("runtime") == 0)
-    //   ) {
-    //     try (Scanner sc = new Scanner(System.in)) {
-    //       System.out.print("Enter the Height: ");
-    //       int height = sc.nextInt();
-    //       System.out.print("Enter the character: ");
-    //       char symbol = sc.next().charAt(0);
-    //       pattern(height, symbol);
-    //     } catch (Exception e) {
-    //       System.out.println(e);
-    //     }
-    //   } else {
-    //     int height1 = 0;
-    //     char symbol1 = ' ';
-    //     boolean flag = false;
-
-    //     for (int index = 0; index < args.length; index++) {
-    //       if (
-    //         args[index].compareTo("-h") == 0 ||
-    //         args[index].compareTo("-height") == 0
-    //       ) {
-    //         height1 = Integer.parseInt(args[index + 1]);
-    //       } else if (
-    //         args[index].compareTo("-s") == 0 ||
-    //         args[index].compareTo("-symbol") == 0
-    //       ) {
-    //         if (args[index + 1].length() == 1) {
-    //           symbol1 = args[index + 1].charAt(0);
-    //         } else {
-    //           System.out.println("Enter a Single Character \n");
-    //           flag = true;
-    //           break;
-    //         }
-    //       } else {
-    //         System.out.println(args[index] + " is not a valid option \n");
-    //         flag = true;
-    //         break;
-    //       }
-    //     }
-    //     if (flag) {
-    //       commandOptions();
-    //     } else {
-    //       pattern(height1, symbol1);
-    //     }
-    //   }
-    // } else {
-    //   commandOptions();
-    // }
-
-    // Without implementing try-with-resource block we can close the resource manually
-    // by declaring the resource variable as null outside try-catch block
-    // and we can close the resource in the finally block as follows
-
-    // Scanner sc = null;
-    // try {
-    //     sc = new Scanner(System.in);
-    //     System.out.print("Enter the Height: ");
-    //     int height = sc.nextInt();
-    //     System.out.print("Enter the character: ");
-    //     char symbol = sc.next().charAt(0);
-    //     pattern(height, symbol);
-    // } catch(Exception e) {
-    //     System.out.println(e);
-    // } finally {
-    //     sc.close();
-    // }
   }
 
   public static void pattern(int height, char symbol) {
@@ -100,78 +26,82 @@ class Pattern {
   }
 
   public static void commandOptions() {
+    System.out.println("\nFollow the following options to explore");
     System.out.println(
-      "No Such Command Found \nFollow the following options to explore"
+      "\n java Pattern -i runtime (for user interface) \n java Pattern -install runtime (for user interface) "
     );
-    System.out.println("\n java Pattern -i runtime (for user interface) \n java Pattern -install runtime (for user interface) ");
     System.out.println(
-      "\n java Pattern -h <integer> -s <character> \n java Pattern -height <integer> -symbol <character> \n"
+      " java Pattern -h <integer> -s <character> \n java Pattern -height <integer> -symbol <character> \n"
     );
   }
 
   public static Configuration argument2Configuration(String args[]) {
     Configuration config = new Configuration();
 
-    // for(String str : args) {
-    //   System.out.println(str);
-    // }
+    int height = 0;
+    char symbol = 0;
 
-    if (args.length != 0) {
+    if (args.length == 0 || args.length % 2 == 1 || args.length > 4) {
+      System.out.println("Invalid Command");
+      commandOptions();
+    }
+
+    if (args.length == 2) {
       if (
         (args[0].compareTo("-i") == 0 || args[0].compareTo("-input") == 0) &&
         (args.length != 1 && args[1].compareTo("runtime") == 0)
       ) {
         try (Scanner sc = new Scanner(System.in)) {
           System.out.print("Enter the Height: ");
-          int height = sc.nextInt();
+          height = sc.nextInt();
           System.out.print("Enter the character: ");
-          char symbol = sc.next().charAt(0);
-          // pattern(height, symbol);
+          symbol = sc.next().charAt(0);
           config.setHeight(height);
           config.setSymbol(symbol);
         } catch (Exception e) {
           System.out.println(e);
         }
-      } else if (args.length == 4) {
-        int height1 = 0;
-        char symbol1 = ' ';
-        boolean flag = false;
+      } else {
+        System.out.println("Invalid Command");
+        commandOptions();
+      }
+    }
 
-        for (int index = 0; index < args.length; index += 2) {
-          if (
-            args[index].compareTo("-h") == 0 ||
-            args[index].compareTo("-height") == 0
-          ) {
-            height1 = Integer.parseInt(args[index + 1]);
-          } else if (
-            args[index].compareTo("-s") == 0 ||
-            args[index].compareTo("-symbol") == 0
-          ) {
-            if (args[index + 1].length() == 1) {
-              symbol1 = args[index + 1].charAt(0);
-            } else {
-              System.out.println("Enter a Single Character \n");
-              flag = true;
-              break;
-            }
-          } else {
-            System.out.println(args[index] + " is not a valid option \n");
-            flag = true;
+    if (args.length == 4) {
+      if (args[0].compareTo("-h") == 0 || args[0].compareTo("-height") == 0) {
+        boolean check = true;
+        for (int index = 0; index < args[1].length(); index++) {
+          if (!Character.isDigit(args[1].charAt(index))) {
+            check = false;
             break;
           }
         }
-        if (flag) {
-          commandOptions();
+        if (check) {
+          height = Integer.parseInt(args[1]);
         } else {
-          // pattern(height1, symbol1);
-          config.setHeight(height1);
-          config.setSymbol(symbol1);
+          System.out.println(args[1] + "Enter a valid integer");
+          commandOptions();
         }
       } else {
+        System.out.println(args[0] + "Invalid Command");
         commandOptions();
       }
-    } else {
-      commandOptions();
+
+      if (args[2].compareTo("-s") == 0 || args[2].compareTo("-symbol") == 0) {
+        if (args[3].length() == 1) {
+          symbol = args[3].charAt(0);
+        } else {
+          System.out.println("Enter a Single Character \n");
+          commandOptions();
+        }
+      } else {
+        System.out.println(args[2] + "Invalid Command");
+        commandOptions();
+      }
+    }
+    if (height != 0 && symbol != 0) {
+      config.setHeight(height);
+      config.setSymbol(symbol);
     }
 
     return config;
